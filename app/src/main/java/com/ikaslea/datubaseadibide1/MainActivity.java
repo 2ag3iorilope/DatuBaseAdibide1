ef51fa6b0f576e653d7d5b6390698dead44e676e
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2025-01-07 09:03:02 +0100

    Initial commit

---
+++ app/src/main/java/com/ikaslea/datubaseadibide1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ikaslea.datubaseadibide1;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UsuariosSQLiteHelper usdbh =
+                new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1); //Version 1
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {
+            //Insertamos 5 usuarios de ejemplo
+            for(int i=1; i<=5; i++)
+            {
+                //Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+                //Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
+                        "VALUES (" + codigo + ", '" + nombre +"')");
+            }
+            //Cerramos la base de datos
+            db.close();
+        }
+    }
+}
